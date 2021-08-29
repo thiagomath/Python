49b94c064abb37787b713d222c198dc13291823c
thiagomath <thiagocruz.mat@gmail.com>
2021-08-29 14:39:45 -0300

    Update ex002.py
    Acrescentei a variável idade e as condições

--- ex002.py
+++ ex002.py
@@ -1,2 +1,10 @@
 nome = str(input('Digite seu nome: '))
 print(f'É um prazer te conhecer {nome}!')
+idade = int(input('Qual é a sua idade? '))
+if idade <= 25:
+    print('Tá novo ainda!')
+elif 25 < idade <= 40:
+    print('Tá ficando velhinho!')
+else:
+    print('Aproveta a vida chefe!')
+    
