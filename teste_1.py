26839360ae1c017bfe11c474b9f2b28dd843b7a1
thiagomath <thiagocruz.mat@gmail.com>
2021-08-30 08:39:42 -0300

    Update teste_1.py
    Testei a função contar números pares entre 0 e n.

--- teste_1.py
+++ teste_1.py
@@ -1,2 +1,9 @@
-n = str(input('Olá, como você se chama? '))
-print(f'Prazer em te conhecer {n}!')
+def conta_numeros(n):
+    p = 0
+    for num in range(n+1):
+        if num%2 == 0:
+            p += 1
+    print(p)
+
+
+conta_numeros(10)
