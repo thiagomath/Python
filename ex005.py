afd1273641c0f4f73312d31f47580775d84ee619
thiagomath <thiagocruz.mat@gmail.com>
2021-09-01 14:02:19 -0300

    Create ex005.py
    Antecessor e sucessor de um número inteiro informado pelo usuário.

---
+++ ex005.py
@@ -0,0 +1,4 @@
+numero = int(input('Digite um número inteiro: '))
+antecessor = numero - 1
+sucessor = numero + 1
+print(f'Analizando o número {numero}, seu antecessor é {antecessor} e seu sucessor é {sucessor}.')
