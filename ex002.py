db21b4127acaeb66c6284fee8c95099a294a2a0b
thiagomath <thiagocruz.mat@gmail.com>
2021-09-08 16:56:05 -0300

    Criei a interface do programa

--- ex002.py
+++ ex002.py
@@ -1,10 +1,24 @@
-nome = str(input('Digite seu nome: '))
-print(f'É um prazer te conhecer {nome}!')
-idade = int(input('Qual é a sua idade? '))
-if idade <= 25:
-    print('Tá novo ainda!')
-elif 25 < idade <= 40:
-    print('Tá ficando velhinho!')
-else:
-    print('Aproveta a vida chefe!')
-    
+from tkinter import *
+
+
+def bt_click():
+    n = nome.get()
+    label_resposta["text"] = f'É um prazer te conhecer {n}!'
+
+
+# Janela
+janela = Tk()
+janela.title("ex001")
+janela.geometry("200x200+10+10")
+# Variáveis
+label_nome = Label(janela, text="Olá, qual é o seu nome? ")
+label_nome.place(x=30, y=20)
+nome = Entry(janela, width=20)
+nome.place(x=30, y=50)
+# Botão
+bt = Button(janela, text="OK", width=10, command=bt_click)
+bt.place(x=50, y=80)
+# Resposta
+label_resposta = Label(janela, text=" ")
+label_resposta.place(x=20, y=120)
+janela.mainloop()
