90567fb30c5a8fbd03c66344392debae8bce87e7
thiagomath <thiagocruz.mat@gmail.com>
2021-09-08 21:48:52 -0300

    Criei a interface do programa

--- ex006.py
+++ ex006.py
@@ -1,12 +1,48 @@
-from math import sqrt
-numero = int(input('Digite um número inteiro: '))
-dobro = 2 * numero
-triplo = 3 * numero
-print(f'Analizando o número {numero}, temos que:')
-print(f'Seu dobro é {dobro},')
-print(f'Seu triplo é {triplo},')
-if numero < 0:
-    print(f'Este número não possui raiz quadrada real.')
-else:
-    raizquad = sqrt(numero)
-    print(f'Sua raiz quadrada é {raizquad}.')
+from tkinter import *
+
+
+def dobro():
+    n = float(valor.get())
+    d = 2 * n
+    label_resposta_dobro["text"] = f'{d:.2f}'
+
+
+def triplo():
+    n = float(valor.get())
+    t = 3 * n
+    label_resposta_triplo["text"] = f'{t:.2f}'
+
+
+def raizquad():
+    from math import sqrt
+    n = float(valor.get())
+    rquad = sqrt(n)
+    label_resposta_raiz_quad["text"] = f'{rquad:.2f}'
+
+
+# Janela
+janela = Tk()
+janela.title("Mini Calculadora")
+janela.geometry("230x200")
+# Variáveis
+# Valor
+label_valor = Label(janela, text="Valor:")
+label_valor.place(x=30, y=20)
+valor = Entry(janela, width=20)
+valor.place(x=70, y=20)
+# Botão dobro
+bt_dobro = Button(janela, text="DOBRO:", width=10, bg="yellow", command=dobro)
+bt_dobro.place(x=30, y=50)
+label_resposta_dobro = Label(janela, text=" ", width=8, borderwidth=1, relief="solid")
+label_resposta_dobro.place(x=120, y=54)
+# Botão triplo
+bt_triplo = Button(janela, text="TRIPLO:", width=10, bg="green", command=triplo)
+bt_triplo.place(x=30, y=80)
+label_resposta_triplo = Label(janela, text=" ", width=8, borderwidth=1, relief="solid")
+label_resposta_triplo.place(x=120, y=84)
+# Botão raiz quadrada
+bt_raiz_quad = Button(janela, text="RAIZ QUADRADA:", width=14, bg="red", command=raizquad)
+bt_raiz_quad.place(x=30, y=110)
+label_resposta_raiz_quad = Label(janela, text=" ", width=8, borderwidth=1, relief="solid")
+label_resposta_raiz_quad.place(x=148, y=114)
+janela.mainloop()
